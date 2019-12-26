f732918b840aa43124e506a0cd4cc7a131adf7c3
gulshan kumar <gulhan@123>
2019-12-26 17:37:17 +0530

    Refactored loadFirstTenLines
    returning a prticular error for all error other than file missing error
    
    so far explored that the head return particular error as mentioned above
    
    function won't return undefined in instead of error

--- src/headLib.js
+++ src/headLib.js
@@ -1,14 +1,9 @@
 const loadFirstTenLines = function(err, data) {
   if (err) {
     const fileError = `head: ${this.filePath}: No such file or directory`;
-    const directoryError = `head: Error reading ${this.filePath}`;
-    const bigFileError = directoryError;
-    const errors = {
-      ENOENT: fileError,
-      EISDIR: directoryError,
-      ERR_FS_FILE_TOO_LARGE: bigFileError
-    };
-    return this.showError(errors[err.code]);
+    const readingError = `head: Error reading ${this.filePath}`;
+    const error = err.code == "ENOENT" ? fileError : readingError;
+    return this.showError(error);
   }
   const firstTenLines = data.split("\n").slice(0, 10);
   return this.showLines(firstTenLines.join("\n"));
