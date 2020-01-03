7eafcdc8a57fab1bf9d84a51b6e5161ff1f41d39
gulshan kumar <gulhan@123>
2020-01-03 09:48:15 +0530

    Adding pre commit and pre push file.

--- addGitHooks.sh
+++ addGitHooks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat <<EOF > .git/hooks/pre-commit  
-eslint src test *.js
+npx eslint src test *.js && mocha && npm run appTests
 if [ \$? != 0 ]; then 
     exit 1
 fi
@@ -11,7 +11,7 @@ EOF
 chmod +x .git/hooks/pre-commit  
 
 cat <<EOF > .git/hooks/pre-push  
-npm run lint 
+npx eslint src test *.js && mocha && npm run appTests
 if [ \$? != 0 ]; then 
     exit 1
 fi
--- head.js
+++ head.js
@@ -1,12 +1,13 @@
-const fs = require("fs");
-const { head } = require("./src/pseudoMain");
+const fs = require('fs');
+const {head} = require('./src/pseudoMain');
 
 const displayOutput = {
   showError: error => process.stderr.write(error),
   showLines: lines => process.stdout.write(lines)
 };
 
-const main = function(userArgs) {
+const main = function (cmdLineArgument) {
+  const [, , userArgs] = cmdLineArgument;
   head(userArgs, fs, displayOutput);
 };
-main(process.argv.slice(2));
+main(process.argv);
