f6a67a07e60c06c979cc22b63f43cea178af5975
gulshan kumar <gulhan@123>
2020-01-04 09:41:18 +0530

    Refactored head function
    handling options by parse function
    
    to reduce responsibility from head function

--- src/pseudoMain.js
+++ src/pseudoMain.js
@@ -1,23 +1,11 @@
-const {separateOptionsAndFiles} = require('./interpretationOfUserArgs');
-const {validateOptionAndField} = require('./validationLib');
+const {parseUserArguments} = require('./interpretationOfUserArgs');
 const {getFirstNLines} = require('./headLib');
 
-const optionHandler = function (options) {
-  let error;
-  options.every(option => {
-    const errorStatement = validateOptionAndField(option);
-    error = errorStatement.error;
-    return errorStatement.validity;
-  });
-  return error;
-};
-
 const head = function (userArgs, fileSys, displayOutput) {
-  const {options, files} = separateOptionsAndFiles(userArgs);
+  const {options, files, optionError} = parseUserArguments(userArgs);
   const firstOptionIndex = 0;
   let numberOfLines = 10;
   if (options.length > firstOptionIndex) {
-    const optionError = optionHandler(options);
     if (optionError) {
       return displayOutput.showError(optionError);
     }
